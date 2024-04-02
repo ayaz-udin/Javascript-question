dcca8588654937adafccf4f01987750be610e777
ayaz-ud-din <ayazuddinmcs@gmail.com>
2024-04-02 14:59:10 +0500

    commited

---
+++ index.js
@@ -0,0 +1,8 @@
+
+// 2. Write a JavaScript program to copy a string to the clipboard.
+
+let stringtext ="I am a text copied to clipboard";
+
+function copybtn(){
+   console.log ("Pakistan zinda bad");
+}
